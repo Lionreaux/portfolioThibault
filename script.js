283b7dfce00f0ff7a14a28f62db6e8b04168f2fe
thibault <tybo2803@hotmail.fr>
2023-03-14 17:04:32 +0100

    commit de la mise a jour du 14 mars

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-// for removing the `pristine` class that prevents CSS animation on load
 document.addEventListener("click",e => {
     let tar = e.target;
     if (tar.name == "toggle")
       tar.removeAttribute("class");
+  });
+
+  function telecharge(nom) {
+    var lienTelechargement = document.createElement("a");
+    lienTelechargement.href = "fichiers/" + nom + ".pdf";
+    lienTelechargement.download = nom + ".pdf";
+    document.body.appendChild(lienTelechargement);
+    lienTelechargement.click();
+    lienTelechargement.addEventListener("load", function() {
+      document.body.removeChild(lienTelechargement);
+    });
+  }
+  
+  document.addEventListener("keydown", function(event) {
+    if (event.key === 'ArrowRight') {
+      window.location.href = "projets.html";
+    }
   });
---
+++ scriptProjets.js
@@ -0,0 +1,12 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'ArrowRight') {
+      window.location.href = "CV.pdf";
+    }
+  });
+  
+document.addEventListener("keydown", function(event) {
+    if (event.key === 'ArrowLeft') {
+      window.location.href = "index.html";
+    }
+  });
+  
